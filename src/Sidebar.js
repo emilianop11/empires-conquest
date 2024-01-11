21d0060792da6c51a2b1ce0cd9a88286e4f05629
Emiliano Pelliccioni <emilianop11@gmail.com>
2024-01-11 00:59:59 -0300

    more images for pieces

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,11 +6,13 @@ import './Sidebar.css';
   
 const Sidebar = ({ onElementClick, setHoverInfo }) => {
     const pieces = [
-        { initial: 'P', name: 'Pawn', description: 'Basic infantry unit.', attack: 1, defense: 1, range: 1 },
-        { initial: 'K', name: 'King', description: 'The most important unit.', attack: 3, defense: 5, range: 1 },
-        { initial: 'C', name: 'Cavalry', description: 'Fast and agile.', attack: 3, defense: 2, range: 1 },
-        { initial: 'A', name: 'Archer', description: 'Ranged unit.', attack: 2, defense: 1, range: 2 },
-        { initial: 'W', name: 'Worker', description: 'Collects resources.', attack: 0, defense: 1 }
+        { initial: 'P', name: 'Pawn', description: 'Basic infantry unit.', attack: 1, defense: 1, range: 1, image: "pawn.png" },
+        { initial: 'K', name: 'King', description: 'The most important unit.', attack: 3, defense: 5, range: 1, image: "king.png" },
+        { initial: 'H', name: 'Horseman', description: 'Fast and agile.', attack: 3, defense: 2, range: 1, image: "horseman.png" },
+        { initial: 'A', name: 'Archer', description: 'Ranged unit.', attack: 2, defense: 1, range: 2, image: "archer.png" },
+        { initial: 'W', name: 'Worker', description: 'Collects resources.', attack: 0, defense: 1, image: "worker.png" },
+        { initial: 'AR', name: 'Archery', description: 'Allows spawning Archers', attack: 0, defense: 1, image: "archery.png" },
+        { initial: 'CA', name: 'Cavalry', description: 'Allows spawning Horsemen.', attack: 0, defense: 1,  image: "cavalry.png" },
     ];
 
   return (
@@ -23,7 +25,8 @@ const Sidebar = ({ onElementClick, setHoverInfo }) => {
                 description: piece.description,
                 attack: piece.attack,
                 defense: piece.defense,
-                range: piece.range
+                range: piece.range,
+                image: piece.image
                 // Image and cost can be added when available
               })}
             onMouseLeave={() => setHoverInfo('')}
